cb1a4981025f1f6f69f37b67441fc97ab4819b99
Murilo Cordeiro <muriloramoz@hotmail.com>
2025-10-16 22:20:55 -0300

    fifth commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -43,14 +43,7 @@ export default function LoginPage() {
       */}
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          />
-          <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Portal Plenitude
-          </h2>
+          <img src="/plenitude.jpg" alt="Plenitude" width={700} height={380} />
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px] shadow-2xl">
