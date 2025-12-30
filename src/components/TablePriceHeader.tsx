4348dbed62c4b21eec1d559d3b385037c5980558
Julio Cesar <julio.wisnieski@gmail.com>
2025-12-30 13:57:24 -0300

    conflit fix

--- src/components/TablePriceHeader.tsx
+++ src/components/TablePriceHeader.tsx
@@ -462,11 +462,7 @@ export default function TablePriceHeader({
       <button
         onClick={onOpenModal}
         disabled={selectedCount === 0}
-<<<<<<< HEAD
-        className={`px-3 py-2  rounded-md text-sm font-medium  cursor-pointer transition-colors flex items-center gap-2 ${selectedCount > 0
-=======
         className={`px-4 py-2 rounded-md text-sm font-medium h-10 cursor-pointer transition-colors flex items-center gap-2 ${selectedCount > 0
->>>>>>> cac04206a31f8d28067d46f65b29789aadedd7cf
           ? "bg-indigo-600 text-white hover:bg-indigo-700"
           : "bg-gray-300 text-gray-500 cursor-not-allowed"
           }`}
